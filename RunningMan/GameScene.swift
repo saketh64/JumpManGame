bc82898c082add98b6f7a053d6b4a98c9b598dee
Saketh Undurty <Saketh@Sakeths-MacBook-Pro.local>
2015-06-15 12:58:20 -0500

    asldkjk in gamescene.swift SU

--- RunningMan/GameScene.swift
+++ RunningMan/GameScene.swift
@@ -15,7 +15,6 @@ class GameScene: SKScene {
         myLabel.text = "Hello, World!";
         myLabel.fontSize = 65;
         myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
-        asldkjk
         self.addChild(myLabel)
     }
     
